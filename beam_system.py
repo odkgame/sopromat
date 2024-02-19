b183a65c217755611a7ce158e95da55b9449db7a
Kucaev <odkgame@mail.ru>
2024-02-19 13:39:22 +0300

    продолжаю работу

--- beam_system.py
+++ beam_system.py
@@ -2,6 +2,7 @@
 
 from Main import DcosFramework
 
+
 # DcosFramework = DlRE
 
 E1: int = 2 * 10 ** 4
@@ -40,15 +41,15 @@ def DelR(L, E, I, F) -> np.array:
 # полная dot delta для рамы
 # L(i,j) - cos и sin для i-го КЭ, L(i,i) -длина i-го КЭ
 def DTRE(L) -> np.array:
-    r = np.array([[L(1, 2), L(1, 3), 0, -L(1, 2), -L(1, 3), 0],
-                  [L(1, 3), -L(1, 2), -L(1, 1) / 2, -L(1, 3), L(1, 2), -L(1, 1) / 2],
+    r = np.array([[L[0][1], L[0][2], 0, -L[0][1], -L[0][2], 0],
+                  [L[0][2], -L[0][1], -L[0][0] / 2, -L[0][2], L[0][1], -L[0][0] / 2],
                   [0, 0, 1, 0, 0, -1]])
     return r
 
 
 j = 0
 W = np.zeros((n, n))
-for i in range(0, 3 * n, 3):
+for i in range(1, 3 * n, 3):
     W[j, 0] = j + 1
     W[j, 1] = i
     W[j, 2] = i + 1
@@ -59,11 +60,30 @@ for i in range(0, 3 * n, 3):
 S = np.array([[1, 2, 3, 4, 5, 6], [4, 5, 6, 7, 8, 9],
               [1, 2, 3, 7, 8, 9], [7, 8, 9, 10, 11, 12]])
 
-b_test = np.zeros((n, n))
+b_test = []
 j = 0
-#for i in range(1, 3 * n, 3):
-    #b_test[i][i+2] = DelR(L=Le[j], E=E[j], I=I[j], F=F[j])
-    #j += 1
+for i in range(1, 3 * n, 3):
+    b_test[i:i + 2] = DelR(L=Le[j][0], E=E[j], I=I[j], F=F[j])
+    j += 1
+
+B = np.diag(b_test)
+B_sub = np.linalg.inv(B)
+
+Rp = np.zeros((3 * n, 12))
+
+for i in range(1, n - 1):
+    Rbl = DTRE(L=Le)
+    for t in range(1, 3):
+        for k in range(1, 6):
+            Rp[int(W[i, t])][int(S[i, k])] = Rp[int(W[i, t])][int(S[i][k])] + Rbl[t][k]
+
+print(Rp)
+
+
+
+
+
+
 
 
 
