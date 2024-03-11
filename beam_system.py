16a6b8e78d555f833ba59dfb563fea63c04b8787
Sergei Kutsaev <odkgame@mail.ru>
2024-03-11 13:32:52 +0300

    написал комменты

--- beam_system.py
+++ beam_system.py
@@ -1,8 +1,8 @@
 ﻿import numpy as np
-#privet
 
 from Main import DcosFramework
 
+# Изменение отображения матриц при print()
 np.set_printoptions(precision=4, suppress=True)
 
 # DcosFramework = DlRE
@@ -42,6 +42,7 @@ def DelR(L, E, I, F) -> np.array:
 
 # полная dot delta для рамы
 # L(i,j) - cos и sin для i-го КЭ, L(i,i) -длина i-го КЭ
+
 def DTRE(L) -> np.array:
     if len(np.shape(L)) == 2:
         r = np.array([[L[0][1], L[0][2], 0, -L[0][1], -L[0][2], 0],
@@ -65,6 +66,7 @@ for i in range(4):
 S = np.array([[1, 2, 3, 4, 5, 6], [4, 5, 6, 7, 8, 9],
               [1, 2, 3, 7, 8, 9], [7, 8, 9, 10, 11, 12]])
 
+# Отнимаю 1 от всех значение из матриц S и W, потому что в питоне отсчет начинается с 0
 W -= 1
 S -= 1
 
@@ -73,8 +75,9 @@ j = 0
 for i in range(1, 3 * n, 3):
     b_test = np.append(b_test, DelR(L=Le[j][0], E=E[j], I=I[j], F=F[j]))
     j += 1
-
+#размещаем элементы на главной диагонали
 B = np.diag(b_test)
+#обращаем мэтот блок МО для использованиия в дальшейших вычислениях
 B_sub = np.linalg.inv(B)
 
 Rp = np.zeros((3 * n, 12))
@@ -86,10 +89,17 @@ for i in range(4):
         for k in range(6):
             Rp[int(W[i, t])][int(S[i, k])] += Rbl[t][k]
 
+
+
+# редуцируем матрицу dot delta (исключаем уравнения для заведомо равхых нулю неизвестных,..
+# соответственно исключаем их из других уравненй)..
+# в матричном виде это соответствует исключению соотвестствующих строк и стобцов
+# в конкретном случае нужно оставить стобцы 4-9 и 12..
+# что мы и делаем
 R = np.array(Rp[:, 3:9])
 R_sub = np.array(Rp[:, 11:12])
 R = np.concatenate((R, R_sub), axis=1)
-
+#теперь дя получения блока dot r транспонируем полученную матрицу, попутно домножив её на -1
 C = np.transpose(R)
 C1 = C * (-1)
 K = np.dot(C1, B_sub)
@@ -110,6 +120,3 @@ x = np.linalg.solve(D, P)
 y = np.linalg.solve(K, P2)
 for i in range(len(x)):
     print(x[i])
-
-
-
